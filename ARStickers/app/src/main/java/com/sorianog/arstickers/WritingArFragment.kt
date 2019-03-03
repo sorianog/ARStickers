77138bcd7a55eab70dd3c158d81065b2fd4b6798
sorianog <13261711+sorianog@users.noreply.github.com>
2019-03-03 07:12:54 -0500

    Start implementing photo capture

---
+++ ARStickers/app/src/main/java/com/sorianog/arstickers/WritingArFragment.kt
@@ -0,0 +1,20 @@
+package com.sorianog.arstickers
+
+import android.Manifest
+import com.google.ar.sceneform.ux.ArFragment
+
+class WritingArFragment : ArFragment() {
+
+    override fun getAdditionalPermissions(): Array<String> {
+        val additionalPermissions = super.getAdditionalPermissions()
+        val permissionLength = additionalPermissions?.size ?: 0
+        val permissions = arrayOfNulls<String>(permissionLength + 1)
+
+        permissions[0] = Manifest.permission.WRITE_EXTERNAL_STORAGE
+        if (permissionLength > 0) {
+            System.arraycopy(additionalPermissions, 0, permissions, 1, additionalPermissions.size)
+        }
+
+        return additionalPermissions
+    }
+}
